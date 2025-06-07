f3b62517624a41827b95f7010abcbdffb4a53965
Minji Kil <yellowminji@naver.com>
2025-06-08 01:52:24 +0900

    [ADD] Mouse Click Movement
    25/06/08
    -- 마우스 좌클릭 시 해당 좌표로 이동

---
+++ Assets/Scripts/Define.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class Define
+{
+    public enum MouseEvent
+    {
+        Press,
+        Click,
+    }
+    public enum CameraMode
+    {
+        QuaterView,
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,16 +3,50 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] private float _speed = 10.0f;
+
+    private bool _moveToDest = false;
+    private Vector3 _destPos;
     void Start()
     {
         Managers.Input.KeyAction -= OnKeyboard; // 중복 실행 방지
         Managers.Input.KeyAction += OnKeyboard;
-
-        Managers.Resource.Instantiate("Tank");
+        
+        Managers.Input.MouseAction -= OnMouseClicked;
+        Managers.Input.MouseAction += OnMouseClicked;
     }
 
-   void Update()
+    void Update()
     {
+        if (_moveToDest)
+        {
+            Vector3 dir = _destPos - transform.position;
+            if (dir.magnitude < 0.0001f)
+            {
+                _moveToDest = false;
+            }
+            else
+            {
+                float moveDist = Mathf.Clamp(_speed * Time.deltaTime, 0, dir.magnitude);
+                transform.position += dir.normalized * moveDist;
+                transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(dir), 10 * Time.deltaTime);
+            }
+        }   
+    }
+
+    void OnMouseClicked(Define.MouseEvent evt)
+    {
+        if (evt != Define.MouseEvent.Click)
+            return;
+        
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        Debug.DrawRay(Camera.main.transform.position, ray.direction * 100f, Color.red, 1.0f);
+        RaycastHit hit;
+        
+        if (Physics.Raycast(ray, out hit, 100.0f, LayerMask.GetMask("Wall")))
+        {
+            _destPos = hit.point;
+            _moveToDest = true;
+        }
         
     }
 
@@ -41,5 +75,7 @@ public class PlayerController : MonoBehaviour
             transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(Vector3.right), _speed * Time.deltaTime);
             transform.position += Vector3.right *  (_speed * Time.deltaTime);
         }
+
+        _moveToDest = false;
     }
 }
