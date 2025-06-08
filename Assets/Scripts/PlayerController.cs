16390c71e27ca177f972b704cc809d74b783a71a
Minji Kil <yellowminji@naver.com>
2025-06-09 01:54:30 +0900

    [ADD] Wait, Run Animation
    25/06/09
    -- Idle, Moving, Die 스테이트에 따른 Update문 분길처리
    -- Wait, Run 애니메이션 추가

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,78 +4,114 @@ public class PlayerController : MonoBehaviour
 {
     [SerializeField] private float _speed = 10.0f;
 
-    private bool _moveToDest = false;
     private Vector3 _destPos;
+
     void Start()
     {
-        Managers.Input.KeyAction -= OnKeyboard; // 중복 실행 방지
-        Managers.Input.KeyAction += OnKeyboard;
-        
         Managers.Input.MouseAction -= OnMouseClicked;
         Managers.Input.MouseAction += OnMouseClicked;
     }
 
+    public enum PlayerState
+    {
+        Die,
+        Moving,
+        Idle,
+    }
+
+    [SerializeField] PlayerState _state = PlayerState.Idle;
+
+    void UpdateDie()
+    {
+    }
+
+    void UpdateMoving()
+    {
+        Vector3 dir = _destPos - transform.position;
+        if (dir.magnitude < 0.0001f)
+        {
+            _state = PlayerState.Idle;
+        }
+        else
+        {
+            float moveDist = Mathf.Clamp(_speed * Time.deltaTime, 0, dir.magnitude);
+            transform.position += dir.normalized * moveDist;
+            transform.rotation =
+                Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(dir), 10 * Time.deltaTime);
+        }
+
+        // 애니메이션
+        Animator anim = GetComponent<Animator>();
+        anim.SetFloat("speed", _speed);
+    }
+
+    void UpdateIdle()
+    {
+        // 애니메이션
+        Animator anim = GetComponent<Animator>();
+        anim.SetFloat("speed", 0);
+    }
+
     void Update()
     {
-        if (_moveToDest)
+        switch (_state)
         {
-            Vector3 dir = _destPos - transform.position;
-            if (dir.magnitude < 0.0001f)
-            {
-                _moveToDest = false;
-            }
-            else
-            {
-                float moveDist = Mathf.Clamp(_speed * Time.deltaTime, 0, dir.magnitude);
-                transform.position += dir.normalized * moveDist;
-                transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(dir), 10 * Time.deltaTime);
-            }
-        }   
+            case PlayerState.Die:
+                UpdateDie();
+                break;
+            case PlayerState.Idle:
+                UpdateIdle();
+                break;
+            case PlayerState.Moving:
+                UpdateMoving();
+                break;
+        }
     }
 
     void OnMouseClicked(Define.MouseEvent evt)
     {
-        if (evt != Define.MouseEvent.Click)
+        if (_state == PlayerState.Die)
             return;
-        
+
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
         Debug.DrawRay(Camera.main.transform.position, ray.direction * 100f, Color.red, 1.0f);
         RaycastHit hit;
-        
+
         if (Physics.Raycast(ray, out hit, 100.0f, LayerMask.GetMask("Wall")))
         {
             _destPos = hit.point;
-            _moveToDest = true;
+            _state = PlayerState.Moving;
         }
-        
     }
 
     void OnKeyboard()
     {
         if (Input.GetKey(KeyCode.W))
         {
-            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(Vector3.forward), _speed * Time.deltaTime);
-            transform.position += Vector3.forward *  (_speed * Time.deltaTime);
-        }
-        
-        if (Input.GetKey(KeyCode.A))
-        {
-            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(Vector3.left), _speed * Time.deltaTime);
-            transform.position += Vector3.left *  (_speed * Time.deltaTime);
-        }
-        
-        if (Input.GetKey(KeyCode.S))
-        {
-            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(Vector3.back), _speed * Time.deltaTime);
-            transform.position += Vector3.back *  (_speed * Time.deltaTime);
-        }
-        
-        if (Input.GetKey(KeyCode.D))
-        {
-            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(Vector3.right), _speed * Time.deltaTime);
-            transform.position += Vector3.right *  (_speed * Time.deltaTime);
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(Vector3.forward),
+                _speed * Time.deltaTime);
+            transform.position += Vector3.forward * (_speed * Time.deltaTime);
         }
 
-        _moveToDest = false;
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(Vector3.left),
+                _speed * Time.deltaTime);
+            transform.position += Vector3.left * (_speed * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(Vector3.back),
+                _speed * Time.deltaTime);
+            transform.position += Vector3.back * (_speed * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(Vector3.right),
+                _speed * Time.deltaTime);
+            transform.position += Vector3.right * (_speed * Time.deltaTime);
+        }
     }
 }
